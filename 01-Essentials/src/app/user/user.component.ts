c632db6e60a9af128ba35b9491d6f891562ef9e0
victor loginov <tkoe88@zoho.com>
2025-04-06 02:03:18 +0200

    set up some class functions for data binding

--- 01-Essentials/src/app/user/user.component.ts
+++ 01-Essentials/src/app/user/user.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { USERS } from '../../data/users';
 
-const rIndex = Math.floor(Math.random() * USERS.length);
-
 @Component({
      selector: 'app-user',
       imports: [],
   templateUrl: './user.component.html',
      styleUrl: './user.component.scss',
 })
+
 export class UserComponent {
-  user = USERS[rIndex];
+  private static random() {
+    const index = Math.floor(Math.random() * USERS.length);
+    return USERS[index];
+  }
+
+  user = UserComponent.random();
+
+  // get: defines a getter method, so imgPath acts like a property, not a function
+  get imgPath() {
+    return `/users/${this.user.img}`;
+  }
+
+  selectUser() {
+    this.user = UserComponent.random();
+  }
 }
