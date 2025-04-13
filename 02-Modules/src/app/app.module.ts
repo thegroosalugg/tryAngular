2ed792f8ba45fb2952881ae7f895631b7a6d87be
victor loginov <tkoe88@zoho.com>
2025-04-13 03:37:02 +0200

    added tasks modules

--- 02-Modules/src/app/app.module.ts
+++ 02-Modules/src/app/app.module.ts
@@ -4,23 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UsersComponent } from './users/users.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskFormComponent } from './tasks/task-form/task-form.component';
 import { ModalComponent } from './modal/modal.component';
+import { TasksModule } from './tasks/tasks.module';
 
 // declares ngModule components
 const declarations = [
        AppComponent,
     HeaderComponent,
      UsersComponent,
-     TasksComponent,
-  TaskFormComponent,
      ModalComponent,
 ];
 // declares standalone components
 const imports = [
   BrowserModule, // required for all Angular apps that use NgModules
     FormsModule, // modules are now imported here instead of in the component
+    TasksModule, // grouped components are added as modules here instead of declarations
 ];
 
 @NgModule({
