335ef91bf75037b12b7a2e807dc200082081ee2b
victor loginov <tkoe88@zoho.com>
2025-04-14 15:54:44 +0200

    init code added

---
+++ 03-InvestmentCalculator/src/data/investment-results.ts
@@ -0,0 +1,32 @@
+function calculateInvestmentResults({
+  initialInvestment,
+           duration,
+     expectedReturn,
+   annualInvestment,
+}: {
+  initialInvestment: number;
+           duration: number;
+     expectedReturn: number;
+   annualInvestment: number;
+}) {
+  const annualData = [];
+  let valueEndOfYear = initialInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const          year = i + 1;
+    const      interest = valueEndOfYear * (expectedReturn / 100);
+        valueEndOfYear += interest + annualInvestment;
+    const totalInterest = valueEndOfYear - annualInvestment * year - initialInvestment;
+
+    annualData.push({
+                     year,
+                 interest,
+           valueEndOfYear,
+         annualInvestment,
+            totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    });
+  }
+
+  return annualData;
+}
