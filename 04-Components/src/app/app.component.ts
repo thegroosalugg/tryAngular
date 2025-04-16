ffa299fc7e8e4bc184bbc26401fb61c294e8471d
victor loginov <tkoe88@zoho.com>
2025-04-17 00:35:52 +0200

    init code added

--- 04-Components/src/app/app.component.ts
+++ 04-Components/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+     selector: 'app-root',
+      imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '04-Components';
+  trafficData = [
+    { id: 'd1',  value: 433 },
+    { id: 'd2',  value: 260 },
+    { id: 'd3',  value: 290 },
+    { id: 'd4',  value: 410 },
+    { id: 'd5',  value: 397 },
+    { id: 'd6',  value: 488 },
+    { id: 'd47', value: 589 },
+  ];
+  maxTraffic = Math.max(...this.trafficData.map((data) => data.value));
+  currentStatus = 'online';
 }
