646474bad8451f9c1a1909dd359b8985c881d851
victor loginov <tkoe88@zoho.com>
2025-04-19 01:09:19 +0200

    updated ticket component

--- 04-Components/src/app/dashboard/tickets/tickets.component.ts
+++ 04-Components/src/app/dashboard/tickets/tickets.component.ts
@@ -10,14 +10,36 @@ import { Ticket } from './ticket.model';
 })
 
 export class TicketsComponent {
-    tickets = signal<Ticket[]>([]);
-  isVisible = signal(false);
+       tickets = signal<Ticket[]>([]);
+  displayState = signal<Record<string, boolean>>({});
 
-  toggleDisplay() {
-    this.isVisible.update(wasVisible => !wasVisible);
+  constructor() {
+    const tickets = localStorage.getItem('tickets');
+    if (tickets) this.tickets.set(JSON.parse(tickets));
+  }
+
+  private save() {
+    localStorage.setItem('tickets', JSON.stringify(this.tickets()));
+  }
+
+  toggleDisplay(id: string) {
+    this.displayState.update((prevState) => ({
+      ...prevState,
+              [id]: !prevState[id], // !toggle open state of selected list item
+    }));
+  }
+
+  closeTicket(ticketId: string) {
+    this.tickets.update((prevState) => {
+      const index = prevState.findIndex(({ id }) => id === ticketId);
+      if (index >= 0) prevState[index].isOpen = false;
+      return prevState;
+    });
+    this.save();
   }
 
   onAdd(ticket: Ticket) {
     this.tickets.update((prev) => [ticket, ...prev]);
+    this.save();
   }
 }
