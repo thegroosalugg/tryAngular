ca09daa21fd2d36a3e5770f16755c49fcc9a96d4
victor loginov <tkoe88@zoho.com>
2025-04-19 23:01:47 +0200

    init code added

---
+++ 06-Pipes/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+const historic = [25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5];
+const current = {
+   berlin: 4.2749812,
+  newYork: 18.1214,
+    paris: 72.1209001,
+  chicago: 65.0775238,
+};
+
+@Component({
+     selector: 'app-root',
+      imports: [],
+  templateUrl: './app.component.html',
+     styleUrl: './app.component.scss',
+})
+export class AppComponent {
+         title = '06-Pipes';
+   currentDate = signal(new Date());
+  temperatures = signal({ current, historic });
+
+  onReset(index: number) {
+    this.temperatures().historic[index] = 18;
+  }
+}
