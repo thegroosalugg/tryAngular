75d298546f9377b2d4e61cfccc0489ca7cc8e827
victor loginov <tkoe88@zoho.com>
2025-04-23 00:58:30 +0200

    connected put/delete

--- 10-HTTPRequests/src/app/places/places.component.ts
+++ 10-HTTPRequests/src/app/places/places.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, input, output, signal } from '@angular/core';
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
 
+const url = 'http://localhost:3000/';
+
 @Component({
      selector: 'app-places',
       imports: [],
@@ -11,20 +13,32 @@ import { HttpClient } from '@angular/common/http';
 export class PlacesComponent {
   private httpClient = inject(HttpClient);
   isUser = input<boolean>(false);
-  select = signal<Place | null>(null);
   places = signal<Place[]>([]);
 
   ngOnInit() { // httpClient is an Observer object
     const endPoint = `${this.isUser() ? 'user-' : ''}places`
     // .get can receive 2nd config arg => { observe: 'response' | 'event }
-    this.httpClient.get<Place[]>('http://localhost:3000/' + endPoint).subscribe({
+    this.httpClient.get<Place[]>(url + endPoint).subscribe({
       // observed .get data is returned here
-      next: (res) => this.places.set(res)
+      next: (res) => this.places.set(res),
     });
   }
 
-  onSelect(place: Place) {
-    this.select.set(place);
-    console.log(place);
+  onSelect(placeId: string) {
+    if (this.isUser()) {
+      // delete a place from user-places
+      this.httpClient
+        .delete<Place[]>(url + 'user-places/' + placeId) // as params
+        .subscribe({
+          next: (res) => this.places.set(res),
+        });
+    } else {
+      // when all places - adds a place to user-places
+      this.httpClient
+        .put<Place[]>(url + 'user-places', { placeId }) // as body
+        .subscribe({
+          next: (res) => console.log(res),
+        });
+    }
   }
 }
