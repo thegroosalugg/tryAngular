0df32f67a7e3023893bb52547a320a13caa1402f
victor loginov <tkoe88@zoho.com>
2025-04-23 05:08:44 +0200

    updated service, cleaned up component

--- 10-HTTPRequests/src/app/places/places.component.ts
+++ 10-HTTPRequests/src/app/places/places.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, input, output, signal } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Place } from './place.model';
-import { HttpClient } from '@angular/common/http';
 import { PlacesService } from './places.service';
 
 @Component({
@@ -10,38 +9,23 @@ import { PlacesService } from './places.service';
      styleUrl: './places.component.scss',
 })
 export class PlacesComponent {
-  isUser = input<boolean>(false);
-  places = signal<Place[]>([]);
-  placesService = inject(PlacesService);
+      isUser = input<boolean>(false);
+  placesServ = inject(PlacesService);
+      places = computed(() =>
+    this.isUser() ? this.placesServ.user() : this.placesServ.all()
+  );
 
   ngOnInit() {
-    // httpClient is an Observer object
-    const endPoint = `${this.isUser() ? 'user-' : ''}places` as const;
-    this.placesService.load(endPoint).subscribe({
-      // observed .get data is returned here
-      next: (res) => {
-        if (this.isUser()) {
-          // separate state for user component instance
-          this.placesService.userPlaces.set(res);
-        } else {
-          this.places.set(res); // main component instance state - won't update after init
-        }
-      },
-    });
+    this.placesServ.load(this.isUser()).subscribe();
   }
 
   onSelect(place: Place) {
     if (this.isUser()) {
       // when user-places component instance - delete a place from user-places
-      this.placesService.remove(place).subscribe({
-        next: (res) => this.placesService.userPlaces.set(res),
-      });
+      this.placesServ.remove(place).subscribe();
     } else {
       // when main component instance for all places - adds a place to user-places
-      this.placesService.add(place).subscribe({
-        next: (res) => // delayed: instant updates cause FS hosted images to fail to load
-          setTimeout(() => this.placesService.userPlaces.set(res), 100),
-      });
+      this.placesServ.add(place).subscribe();
     }
   }
 }
--- 10-HTTPRequests/src/app/places/places.service.ts
+++ 10-HTTPRequests/src/app/places/places.service.ts
@@ -1,29 +1,46 @@
 import { inject, Injectable, signal } from '@angular/core';
-
-import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+import { Place } from './place.model';
 
 @Injectable({ providedIn: 'root' })
 export class PlacesService {
   url = 'http://localhost:3000/';
-  private httpClient = inject(HttpClient);
-  userPlaces = signal<Place[]>([]);
-  // loadedUserPlaces = this.userPlaces.asReadonly();
+  private httpClient = inject(HttpClient); // httpClient is an Observer object
+  private  allPlaces = signal<Place[]>([]);
+  private userPlaces = signal<Place[]>([]);
+   all = this.allPlaces.asReadonly();
+  user = this.userPlaces.asReadonly();
 
-  load(endPoint: 'places' | 'user-places') {
+  load(shouldLoadUser: boolean) {
+    const endPoint = `${shouldLoadUser ? 'user-' : ''}places`;
     // .get can receive 2nd config arg => { observe: 'response' | 'event }
-    return this.httpClient.get<Place[]>(this.url + endPoint);
-  } // return functions so .pipe etc. can be chained
-
-  add(place: Place) {
-    return this.httpClient.put<Place[]>(this.url + 'user-places', {
-      placeId: place.id, // as body
-    });
+    return this.httpClient.get<Place[]>(this.url + endPoint).pipe(
+      // return functions so .pipe etc. can be chained
+      tap({
+        // tap allows running of functions with received data without subscribing
+        next: (res) => {
+          if (shouldLoadUser) {
+            this.userPlaces.set(res); // separate state for user component instance
+          } else {
+            this.allPlaces.set(res); // main component instance state - won't update after init
+          }
+        },
+      })
+    );
   }
 
+  add(place: Place) {
+    return this.httpClient
+      .put<Place[]>(this.url + 'user-places', { placeId: place.id }) // as body
+      .pipe( // delayed: instant updates cause FS hosted images to fail on load
+        tap({ next: (res) => setTimeout(() => this.userPlaces.set(res), 100) })
+      );
+  }
+  
   remove(place: Place) {
-    return this.httpClient.delete<Place[]>(
-      this.url + 'user-places/' + place.id // as params
-    );
+    return this.httpClient
+      .delete<Place[]>(this.url + 'user-places/' + place.id) // as params
+      .pipe(tap({ next: (res) => this.userPlaces.set(res) }));
   }
 }
