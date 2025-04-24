a7a122d080a2f8a23ea6ff252ca8f4222e90e560
victor loginov <tkoe88@zoho.com>
2025-04-24 23:47:32 +0200

    updated login form to show errors & capture data

--- 11-Forms/src/app/auth/login/login.component.ts
+++ 11-Forms/src/app/auth/login/login.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
+// *TEMPLATE DRIVEN APPROACH
 @Component({
      selector: 'app-login',
+      imports: [FormsModule],
   templateUrl: './login.component.html',
      styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(ngForm: NgForm) {
+    const { email, password } = ngForm.controls;
+    // console.dir(ngForm);
+    console.log(email);
+    console.log(password);
+    // console.log(email.status);
+    // console.log(password.status);
+  }
+}
