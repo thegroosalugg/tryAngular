019685237aa1a462c05452ac1cf5706141ecf4dc
victor loginov <tkoe88@zoho.com>
2025-04-25 02:35:47 +0200

    added form saved input data

--- 11-Forms/src/app/auth/login/login.component.ts
+++ 11-Forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 // *TEMPLATE DRIVEN APPROACH
 @Component({
@@ -9,12 +10,42 @@ import { FormsModule, NgForm } from '@angular/forms';
      styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  form = viewChild.required<NgForm>('form'); // capture ngForm with #form #templateVar
+  destroyRef = inject(DestroyRef);
+
+  constructor() {
+    // afterNextRender() defers execution until after the component has rendered once
+    afterNextRender(() => {
+      const savedData = localStorage.getItem('form-data');
+
+      if (savedData) {
+        const email = JSON.parse(savedData).email;
+        // timeout required: though <Form> is initialised, its children controls are not yet in this lifecycle
+        setTimeout(() => {
+          this.form().controls['email'].setValue(email);
+          // this.form().setValue({ email: '', password: '' }); // when changing multiple values
+        }, 1);
+      }
+
+      const subscription = this.form() // valueChanges is an Observable and can be subscribed to
+        .valueChanges?.pipe(debounceTime(500)) // debounceTime piped - better performance
+        .subscribe({
+          next: (value) =>
+            localStorage.setItem(
+              'form-data',
+              JSON.stringify({ email: value.email })
+            ),
+        });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(ngForm: NgForm) {
+    if (ngForm.invalid) return;
     const { email, password } = ngForm.controls;
-    // console.dir(ngForm);
-    console.log(email);
-    console.log(password);
-    // console.log(email.status);
-    // console.log(password.status);
+    console.log(email.value);
+    console.log(password.value);
+    ngForm.form.reset();
   }
 }
