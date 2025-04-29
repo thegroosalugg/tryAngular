614ac8c238f13cbd12e2b2b3ef229f811e7f9e2f
victor loginov <tkoe88@zoho.com>
2025-04-30 01:00:14 +0200

    added app component switcher

--- 11-Forms/src/app/app.component.ts
+++ 11-Forms/src/app/app.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { LoginTemplateComponent } from './auth/login-template-driven/login.template.component';
-import { LoginReactiveComponent } from "./auth/login-reactive/login.reactive.component";
+import { LoginReactiveComponent } from './auth/login-reactive/login.reactive.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { NgComponentOutlet } from '@angular/common';
 
-const imports = [
-  // LoginTemplateComponent,
-  LoginReactiveComponent,
-];
+type CustomComponent =
+  | typeof LoginTemplateComponent
+  | typeof LoginReactiveComponent
+  | typeof SignupComponent;
 
 @Component({
      selector: 'app-root',
-      imports,
+      imports: [NgComponentOutlet],
   templateUrl: './app.component.html',
-     styleUrl: './app.component.scss'
+     styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '11-Forms';
+       title = '11-Forms';
+       forms = signal(['Login-Template', 'Login-Reactive', 'SignUp-Reactive']);
+   component = signal<CustomComponent>(LoginTemplateComponent);
+  components = signal([
+    LoginTemplateComponent,
+    LoginReactiveComponent,
+    SignupComponent,
+  ]);
+
+  switch(index: number) {
+    this.component.set(this.components()[index]);
+  }
 }
