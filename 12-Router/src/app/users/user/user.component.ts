48516e37b76b1c2cce5ace598d8d7936424d7ef0
victor loginov <tkoe88@zoho.com>
2025-05-02 12:18:24 +0200

    added observables invalid userId redirect

--- 12-Router/src/app/users/user/user.component.ts
+++ 12-Router/src/app/users/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
      selector: 'app-user',
@@ -9,17 +9,28 @@ import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
      styleUrl: './user.component.scss'
 })
 export class UserComponent implements OnInit {
-  userId = input.required<string>(); // received via URL - names must match routes/:id
+  // userId = input.required<string>(); // received via URL - names must match routes/:id
   private          users = inject(UsersService);
   private activatedRoute = inject(ActivatedRoute); // *Observable alternative to input()
   private     destroyRef = inject(DestroyRef);
-  userName = computed(() => this.users.find(this.userId())?.name);
+  private         router = inject(Router);
+  userName = signal('');
 
   ngOnInit() {
     const subscription = this.activatedRoute.paramMap.subscribe({
       // subscribe to paramMap to get userId
-      next: (paramsMap) => console.log('User ID:', paramsMap.get('userId')),
-    })
+      next: (paramMap) => {
+        const userId = paramMap.get('userId');
+        if (userId) {
+          const user = this.users.find(userId);
+          if (user) {
+            this.userName.set(user.name);
+            return; // user found - return function early
+          }
+        } // user not found - navigate to root
+        this.router.navigate(['']);
+      },
+    });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
