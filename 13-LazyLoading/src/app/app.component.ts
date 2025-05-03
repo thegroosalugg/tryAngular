ee63b2bab894c8d6b7e457388d6663a2892f6612
victor loginov <tkoe88@zoho.com>
2025-05-03 20:35:05 +0200

    added some extra components

--- 13-LazyLoading/src/app/app.component.ts
+++ 13-LazyLoading/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UsersComponent } from './users/users.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
      selector: 'app-root',
-      imports: [RouterOutlet, HeaderComponent, UsersComponent],
+      imports: [RouterOutlet, HeaderComponent, FooterComponent, UsersComponent],
   templateUrl: './app.component.html',
      styleUrl: './app.component.scss'
 })
---
+++ 13-LazyLoading/src/app/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { OfferPreviewComponent } from "../offer-preview/offer-preview.component";
+
+@Component({
+     selector: 'app-footer',
+  templateUrl: './footer.component.html',
+     styleUrl: './footer.component.scss',
+     imports: [OfferPreviewComponent],
+})
+
+export class FooterComponent {}
